47f02481f710613f351c3518ca764bf2fc065475
Mattia Failla <mattiafailla@hotmail.it>
2021-09-27 09:31:25 +0200

    Improve helper

---
+++ scripts/helper.py
@@ -0,0 +1,70 @@
+from brownie import accounts, network, config, MockV3Agregator, Contract
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+contract_to_mock = {"eth_usd_price_feed": MockV3Agregator,
+                    "vfr_coordinator": }
+
+# MockV3 data
+DECIMALS = 8
+STARTING_PRICE = 2000000000000
+
+
+def get_account(index: int = None, account_id: str = None):
+    if index:
+        return accounts[index]
+    if account_id:
+        return accounts.load(account_id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+
+
+def get_contract(contract_name: str = None):
+    """If you want to use this function, go to the brownie config and add a new entry for
+    the contract that you want to be able to 'get'. Then add an entry in the in the variable 'contract_to_mock'.
+    You'll see examples like the 'link_token'.
+        This script will then either:
+            - Get a address from the config
+            - Or deploy a mock to use for a network that doesn't have it
+        Args:
+            contract_name (string): This is the name that is refered to in the
+            brownie config and 'contract_to_mock' variable.
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            Contract of the type specificed by the dictonary. This could be either
+            a mock or the 'real' contract on a live network.
+    """
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in FORKED_LOCAL_ENVIRONMENTS:
+        if len(contract_type) <= 0:
+            deploy_mocks()
+        contract = contract_type[-1]
+    else:
+        try:
+            contract_address = config["networks"][network.show_active()][contract_name]
+            contract = Contract.from_abi(
+                contract_type._name, contract_address, contract_type.abi
+            )
+        except KeyError:
+            print(
+                f"{network.show_active()} address not found, perhaps you should add it to the config or deploy mocks?"
+            )
+            print(
+                f"brownie run scripts/deploy_mocks.py --network {network.show_active()}"
+            )
+            return
+    return contract
+
+
+def deploy_mocks(decimals=DECIMALS, initial_value=STARTING_PRICE):
+    """
+    Deploying mock contracts
+    """
+    account = get_account()
+    mock_price_feed = MockV3Agregator.deploy(decimals, initial_value, {"from": account})
+    print(f"Deployed mock contract price feed: {mock_price_feed}")
