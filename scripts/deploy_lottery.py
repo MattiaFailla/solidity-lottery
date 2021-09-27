28c401ae4a0e995a1b6f5e1e80d13b9e9e65cb39
Mattia Failla <mattiafailla@hotmail.it>
2021-09-27 10:12:01 +0200

    Start, enter and end a Lottery

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -14,7 +14,39 @@ def deploy_lottery():
         publish_source=config["networks"][network.show_active()].get("verify", False),
     )
     print("Lottery contract deployed!")
+    return lottery_contract
+
+
+def start_lottery():
+    print("Starting lottery...")
+    account = get_account(account_id="learning")
+    lottery = Lottery[-1]
+    start_tx = lottery.startLottery({"from": account})
+    start_tx.wait(1)
+    print("Lottery is started.")
+
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 10000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("Entered the lottery!")
+
+
+def end_lottery():
+    """
+    The contract will revert if no Link token are available in the contract.balance
+    """
+    account = get_account(account_id="learning")
+    lottery = Lottery[-1]
+    tx = lottery.endLottery({"from": account})
+    tx.wait(1)
+    print("Lottery ended!")
 
 
 def main():
     deploy_lottery()
+    start_lottery()
+    enter_lottery()
