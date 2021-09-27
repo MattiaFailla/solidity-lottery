1b4b03332f0f6e42494b882f7e6032011fc90a98
Mattia Failla <mattiafailla@hotmail.it>
2021-09-27 09:40:52 +0200

    Improve deploy
    Set Contract constructor params, improve publish source handling

--- scripts/deploy_lottery.py
+++ scripts/deploy_lottery.py
@@ -1,13 +1,19 @@
 from scripts.helper import get_account, get_contract
-from brownie import Lottery
+from brownie import Lottery, config, network
 
 
 def deploy_lottery():
     account = get_account(account_id="learning")
     lottery_contract = Lottery(
-        get_contract("eth_usd_price_feed").address
-
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vfr_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False),
     )
+
     pass
 
 
